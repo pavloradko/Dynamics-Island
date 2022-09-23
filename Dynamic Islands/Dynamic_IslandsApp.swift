50565c3c420f331b07a135772f8ed768542931fe
unknown <radkop998@gmail.com>
2022-09-24 01:38:44 +0300

    Initial Commit

---
+++ Dynamic Islands/Dynamic_IslandsApp.swift
@@ -0,0 +1,18 @@
+//
+//  Dynamic_IslandsApp.swift
+//  Dynamic Islands
+//
+//  Created by Jordi Bruin on 15/09/2022.
+//
+
+import SwiftUI
+//@_exported import Inject
+
+@main
+struct Dynamic_IslandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
